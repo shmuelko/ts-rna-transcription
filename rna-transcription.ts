9ac8b858e18f739b93869caadad958237c4cd061
shmuelko <shmuel777@gmail.com>
2024-01-07 22:43:49 +0200

    done

--- rna-transcription.ts
+++ rna-transcription.ts
@@ -1,3 +1,14 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(dna: string): string {
+  let new_val: string = "";
+
+  const obj = {"C": "G", "G": "C", "A": "U", "T": "A"};
+  const map_dna: Map<string, string> = new Map<string, string>(Object.entries(obj));
+  for(let r_val of dna) {
+    if (map_dna.has(r_val)) {
+      new_val += map_dna.get(r_val);
+    } else {
+      throw new Error("Invalid input DNA.");
+    }
+  }
+  return new_val;
 }
